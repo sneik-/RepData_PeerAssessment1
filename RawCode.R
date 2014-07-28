e369de826bb67d4060568641e18b65e47ce620b1
sneik- <risto.kaartinen@gmail.com>
2014-07-28 20:17:18 +0300

    Still not much

--- RawCode.R
+++ RawCode.R
@@ -15,7 +15,19 @@ df <- read.csv("activity.csv")
 steps_daily <- aggregate(df$steps, list(df$date), FUN=sum)
 names(steps_daily) <- c("Date","Steps")
 hist(steps_daily$Steps, breaks=30, xlab="Daily steps", main="Histogram of daily steps")
+
+## Calculate and report the mean and median total number of steps taken per day
+
+round(mean(steps_daily$Steps, na.rm=T))
+round(median(steps_daily$Steps, na.rm=T))
  
+# What is the average daily activity pattern?
+
+## Make a time series plot (i.e. type = "l") of the 5-minute interval (x-axis) and 
+## the average number of steps taken, averaged across all days (y-axis)
+
+
+
 # Imputing missing values
 
 ## Calculate and report the total number of missing values in the dataset 
